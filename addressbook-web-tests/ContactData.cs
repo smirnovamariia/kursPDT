d315f3045d397d7f18e8f3e50a2550bd68251412
smirnovamariia <mariya.smirnova@katharsis.ru>
2019-05-03 18:12:25 +0300

    Создание контакта

---
+++ addressbook-web-tests/ContactData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class ContactData
+    {
+        private string firstname;
+        private string lastname;
+        private string nickname;
+        private string title;
+
+        public ContactData (string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+        public string Nickname
+        {
+            get { return nickname; }
+            set { nickname = value; }
+        }
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+    }
+}
